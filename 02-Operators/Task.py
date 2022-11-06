55e950c78c2b7c86aa81cd83f5d2f5315804868e
Danny Nasra <danina964@gmail.com>
2022-11-06 20:58:01 +0100

    the second task is done

---
+++ 02-Operators/Task.py
@@ -0,0 +1,47 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+''' differences between = and == '''
+print ("= : for the variabel's install EX: x=5 , and == : for compare between tow variabels from the same Type ")
+
+ ''' using !='''
+print(" is not equal , to compare between tow variabels")
+
+
+''' Operator Precedence'''
+
+print("the order of the Operator : **-> *,/ -> +,- ")
+
+'''variable with a value'''
+
+x=10
+
+''' increase a value by 15'''
+
+x +=15
+
+'''divide a value by 5'''
+
+x /=5
+
+'''multiply a value by 10'''
+
+x *=10
+
+'''module of x on 3 using assignment Operators'''
+
+x %=3
+
+'''print module of 11 to 4'''
+
+print(11%4)
+
+'''print exponent of 2,3'''
+print(2**3)
+
+'''divide 11 by 3 using Python division'''
+
+11/3
+
+'''can we divide 11 by 3 and get an integer number'''
+
+print(' yes we can ,using // ')
