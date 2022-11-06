bf0c7341034cf919a807714004b86fec648e1d53
Danny Nasra <danina964@gmail.com>
2022-11-06 20:26:18 +0100

    done

--- 01-Variabels/Tasks.py
+++ 01-Variabels/Tasks.py
@@ -1,2 +1,49 @@
 Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+'''# create a Boolean Variable named x'''
+
+x = true
+
+'''# create an integer variable named y'''
+
+y = 1
+
+'''#creat a float variable named z'''
+
+z = 1.1
+
+'''#creat a String varibale named s'''
+
+s = 'Danny'
+
+'''#convert the int variable to float'''
+
+i = 1
+f= float(i)
+
+'''convert String to int ?'''
+ print (' yes we can ,if the string is just a numbers without letters')
+
+ '''create a list of numbers from 1 to 5'''
+
+ l= [1,2,3,4,5]
+
+ '''create a tuple from 10 to 15'''
+ t = (10,11,12,13,14,15)
+
+ '''convert the list to tuple'''
+ 
+tu = tuple(l)
+
+'''create a dict of 3 values'''
+d ={1 : 'danny' , 2 : 'mark' , 3: 'Tony'}
+
+'''use semi colon with Python"'''
+print ('yes ,to write more than one Command in the same Line, (between the commands')
+
+'''Python is interpreted or compiled'''
+print('interpreted')
+
+'''difference between low and High Level'''
+print('the low level is so complex for humens but it is so easily to understand for computer , for high level it is the opposite'
+      'which make the low level faster than the high level ')
